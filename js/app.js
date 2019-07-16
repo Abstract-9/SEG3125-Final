8eda12db440fac5221e3210e42590d4bf541d60e
Logan Rodie <lrodi045@uottawa.ca>
2019-07-16 10:26:55 -0400

    multiple recipe submission

--- js/app.js
+++ js/app.js
@@ -8,10 +8,23 @@ $(document).ready(function() {
 
     if($('meta[name=customPage]').length){
         let db = loadDB();
-        $('#name').text(db.sauce.name);
-        $('#type').text(db.sauce.category);
-        $('#desc').text(db.sauce.desc);
-        $('#tags').text(db.sauce.tags);
+        let targetSauce = null;
+        for(let index in db.sauces) {
+            if(db.sauces[index]===null){
+                delete db.sauces[index];
+                continue;
+            }
+            if (db.sauces[index].name===db.nextSauce){
+                targetSauce = db.sauces[index];
+                break;
+            }
+        }
+        if(targetSauce!==null) {
+            $('#name').text(targetSauce.name);
+            $('#type').text(targetSauce.category);
+            $('#desc').text(targetSauce.desc);
+            $('#tags').text(targetSauce.tags);
+        }
     }else if($('meta[name=accountPage]').length){
         let db = loadDB();
         let acc = getAccounts();
@@ -21,7 +34,10 @@ $(document).ready(function() {
         let userField = $('#user');
         userField.text(user + "'s Account profile");
         $("#sel1").val(acc.level[userIndex]);
-        if(db.sauce) appendSauce($('#sauce2'))
+        for(let index in db.sauces){
+            if(db.sauces[index]===null) continue;
+            appendSauce(db.sauces[index].name)
+        }
     }
 
     if($('p.remove').length){
@@ -84,11 +100,11 @@ function submitSauce(){
     sauce.name = $("#name").val();
     sauce.desc = $("#comment").val();
     sauce.category = $('input[name=exampleRadios]:checked').val();
-    sauce.tags = $('#tags').val().replace(' ', ', ');
+    sauce.tags = $('#tags').val().replace('/\s/g', ', ');
     let db = loadDB();
-    db.sauce = sauce;
+    db.sauces.push(sauce);
     storeDB(db);
-    window.location.href='customRecipe.html'
+    loadsauce(db.sauces[db.sauces.length-1].name);
 }
 
 
@@ -103,7 +119,7 @@ function storeDB(data){
 }
 
 function initData(){
-    return {signedIn: false, user: null}
+    return {signedIn: false, user: null, sauces: []}
 }
 
 function getAccounts(){
@@ -121,17 +137,17 @@ function storeAccounts(acc){
     localStorage.setItem('accounts', JSON.stringify(acc));
 }
 
-function appendSauce(selector){
+function appendSauce(name){
     let db = loadDB();
-    selector.after(`<tr>
+    $('#sauce-parent').append(`<tr>
                 <td>
-                    <button type="button" class="btn btn-outline-secondary m-2 p-2" ><a class ="text-secondary" href = "customRecipe.html">Go to Sauce</a></button>
+                    <button type="button" class="btn btn-outline-secondary m-2 p-2" onclick="loadsauce(name)">Go to Sauce</button>
 
                 </td>
 
                 <td><div class="row">
 
-                    <p style="display:inline" class="pr-2 w-25">${db.sauce.name}</p>
+                    <p style="display:inline" class="pr-2 w-25">${name}</p>
                     <p class="star-black"></p>
                     <p class="star-black"></p>
                     <p class="star-black"></p>
@@ -144,9 +160,24 @@ function appendSauce(selector){
 }
 
 function removePost(){
+    let name = $(this).parent().children()[0].innerText;
     let data = loadDB();
-    data.sauce = null;
+    let targetSauce = null;
+    for(let i=0; i<data.sauces.length;i++){
+        if(data.sauces[i]===null){
+            delete data.sauces[i];
+            continue;
+        }
+        if(data.sauces[i].name===name) targetSauce = i;
+    }
+    delete data.sauces[targetSauce];
     storeDB(data);
     window.location.reload();
 }
 
+function loadsauce(name){
+    let db = loadDB();
+    db.nextSauce = name;
+    storeDB(db);
+    window.location.href = 'customRecipe.html';
+}
